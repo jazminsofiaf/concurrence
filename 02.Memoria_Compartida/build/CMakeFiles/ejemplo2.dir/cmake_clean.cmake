c2dc811c3373f45ed61eb482d52ad7cf83f05b1d
Jazmin Ferreiro <jazminsofiaf@gmail.com>
2020-04-26 22:05:32 -0300

    add shared memory

---
+++ 02.Memoria_Compartida/build/CMakeFiles/ejemplo2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ejemplo2.dir/main2.cc.o"
+  "ejemplo2"
+  "ejemplo2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ejemplo2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
